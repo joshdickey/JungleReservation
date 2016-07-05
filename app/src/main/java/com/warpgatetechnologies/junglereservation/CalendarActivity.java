18e4a88dad6f1c4a1f4e78dd467624ccb2f34f03
jdickey <josh.dickey1@gmail.com>
2016-07-05 16:22:04 -0600

    Add TextView formatting and Intent

--- app/src/main/java/com/warpgatetechnologies/junglereservation/CalendarActivity.java
+++ app/src/main/java/com/warpgatetechnologies/junglereservation/CalendarActivity.java
@@ -1,7 +1,10 @@
 package com.warpgatetechnologies.junglereservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+
     }
+
+
 }
--- app/src/main/java/com/warpgatetechnologies/junglereservation/MainActivity.java
+++ app/src/main/java/com/warpgatetechnologies/junglereservation/MainActivity.java
@@ -1,7 +1,10 @@
 package com.warpgatetechnologies.junglereservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView calendarTextView = (TextView) findViewById(R.id.current_reservations_text_view);
+        calendarTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalendar(view);
+            }
+        });
+
+        TextView requestTextView = (TextView) findViewById(R.id.request_reservation_text_view);
+        requestTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestReservation(view);
+            }
+        });
     }
+
+    public void openCalendar(View view){
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+    public void requestReservation(View view){
+        Intent intent = new Intent(this,ReservationActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
